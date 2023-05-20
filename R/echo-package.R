078139c6ad9a3b9ee209892c33003535934f6b94
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-05-20 14:55:26 -0400

    Create echo-package.R

---
+++ R/echo-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+op.echo <- list(
+  echo.timezone = "UTC"
+)
+
+.onAttach <- function(libname, pkgname) {
+  options(op.echo[!names(op.echo) %in% names(options())])
+}
